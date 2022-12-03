369607f9637e92f72ddd0870f73c26432a053732
LeonardMarian <118011483+LeonardMarian@users.noreply.github.com>
2022-12-03 10:02:45 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+#include "lib.h"
 
+using namespace std;
+
+void EvaluatePrimeNumbers(){
+    int n = 0;
+    cin >> n;
+
+    bool isPrime = is_prime_recorsive(n, n, 0);
+    if(isPrime)
+        cout << "numero primo";
+    else
+        cout << "numero non primo";
+}
