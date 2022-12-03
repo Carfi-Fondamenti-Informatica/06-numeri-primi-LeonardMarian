55835d2d333e120a60cd6c7f8065680e8198e2ae
LeonardMarian <118011483+LeonardMarian@users.noreply.github.com>
2022-12-03 10:01:44 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,13 @@
+bool is_prime_recorsive(int n, int divisore, int contatore){
+    if (n == 1)
+        return true;
+
+    if (n%divisore == 0)
+        contatore ++;
+    int divisoremenouno = divisore-1;
+    if (divisoremenouno == 0)
+        return contatore == 2;
+
+    return is_prime_recorsive(n, divisoremenouno, contatore);
+}
 
